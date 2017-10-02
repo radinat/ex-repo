1251cf1fd5aae6a85793062185d339475f47cb80
radinat <32281618+radinat@users.noreply.github.com>
2017-10-02 23:26:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18DiffIntSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            try
+            {
+                long num = long.Parse(input);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"{input} can't fit in any type")
+            }
+        }
+    }
+}
